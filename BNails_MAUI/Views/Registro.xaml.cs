1e754140f6ae20cb385fe8c1c711a3a83e49e4d4
RafaIvanCz <rafaelivancz@gmail.com>
2025-06-05 20:38:52 -0300

    Se crea interfaz Registro para usuarios nuevos.

--- BNails_MAUI/Views/Registro.xaml.cs
+++ BNails_MAUI/Views/Registro.xaml.cs
@@ -6,4 +6,9 @@ public partial class Registro : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void VolverAlLogin_btn(object sender,EventArgs e)
+    {
+		await Shell.Current.GoToAsync("..");
+    }
 }
