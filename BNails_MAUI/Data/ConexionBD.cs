1d2a003e377dc2b856f3b528dc17daece7ac4ea2
RafaIvanCz <rafaelivancz@gmail.com>
2025-06-13 21:52:07 -0300

    Se finaliza validaciones en Registro para Email, Password y RePassword

---
+++ BNails_MAUI/Data/ConexionBD.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNails_MAUI.Data
+{
+    public class ConexionBD
+    {
+        private const string conexionBD = "server=localhost;port=3306;database=bnails;user=root;password=root";
+
+        public static MySqlConnection ObtenerConexionBD()
+        {
+            var conexion = new MySqlConnection(conexionBD);
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
---
+++ BNails_MAUI/Helpers/SeguridadHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNails_MAUI.Helpers
+{
+    public static class SeguridadHelper
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool VerificarPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
